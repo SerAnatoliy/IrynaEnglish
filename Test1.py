88f3b2f4ff3b6fd9b35a04bf853a867f2bb70ce3
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-05-07 17:26:30 +0200

    iteration2

--- Test1.py
+++ Test1.py
@@ -1,37 +1,55 @@
-answer1= answer
-while answer1 != "True"or answer1 != "False":
+answer1 =''
+answer2 =''
+answer3 =''
+answer4 =''
+answer5 =''
+while True :
     print ("Daniel shares personal information with his employer?")
-    answer= input ()
-    if answer == "True":
+    answer1= input ()
+    if answer1 == "True":
         print ("Wrong")
-    elif answer == "False":
+        break
+    elif answer1 == "False":
         print ("Correct")
+        break
     else : print ("The answer could be only 'True' or 'False'")
-print("Daniel speals about his education and trainig?")
-answer2 = input()
-if answer2 == "True":
-    print ("Wrong")
-elif answer2 == "False":
-    print ("Correct")
-else : print ("The answer could be only 'True' or 'False'")
-print ("In addirion to your answer you may describe one of the projects you have done?")
-answer3 = input()
-if answer3 == "True":
-    print ("Wrong")
-elif answer3 == "False":
-    print ("Correct")
-else : print ("The answer could be only 'True' or 'False'")
-print ("Daniel informs about the reason he wants to work for this company?")
-answer4 = input()
-if answer4 == "True":
-    print ("Correct")
-elif answer4 == "False":
-    print ("Wrong")
-else : print ("The answer could be only 'True' or 'False'")    
-print ("The resons why you want to work for the company should concern only your pesonal development not company's?")
-answer5 = input()
-if answer5 == "True":
-    print ("wrong")
-elif answer5 == "False":
-    print ("Correct")
-else : print ("The answer could be only 'True' or 'False'")    
+while True :
+    print("Daniel speaks about his education and trainig?")
+    answer2 = input()
+    if answer2 == "True":
+        print ("Wrong")
+        break
+    elif answer2 == "False":
+        print ("Correct")
+        break
+    else : print ("The answer could be only 'True' or 'False'")
+while True :
+    print ("In addirion to your answer you may describe one of the projects you have done?")
+    answer3 = input()
+    if answer3 == "True":
+        print ("Wrong")
+        break
+    elif answer3 == "False":
+        print ("Correct")
+        break
+    else : print ("The answer could be only 'True' or 'False'")
+while True :
+    print ("Daniel informs about the reason he wants to work for this company?")
+    answer4 = input()
+    if answer4 == "True":
+        print ("Correct")
+        break
+    elif answer4 == "False":
+        break
+        print ("Wrong")
+    else : print ("The answer could be only 'True' or 'False'")    
+while True :
+    print ("The resons why you want to work for the company should concern only your pesonal development not company's?")
+    answer5 = input()
+    if answer5 == "True":
+        print ("wrong")
+        break
+    elif answer5 == "False":
+        print ("Correct")
+        break
+    else : print ("The answer could be only 'True' or 'False'")    
