89a7f26a95b712fd850d3b9986431bca908c883c
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-05-07 17:06:53 +0200

    iteration1

---
+++ Test1.py
@@ -0,0 +1,37 @@
+answer1= answer
+while answer1 != "True"or answer1 != "False":
+    print ("Daniel shares personal information with his employer?")
+    answer= input ()
+    if answer == "True":
+        print ("Wrong")
+    elif answer == "False":
+        print ("Correct")
+    else : print ("The answer could be only 'True' or 'False'")
+print("Daniel speals about his education and trainig?")
+answer2 = input()
+if answer2 == "True":
+    print ("Wrong")
+elif answer2 == "False":
+    print ("Correct")
+else : print ("The answer could be only 'True' or 'False'")
+print ("In addirion to your answer you may describe one of the projects you have done?")
+answer3 = input()
+if answer3 == "True":
+    print ("Wrong")
+elif answer3 == "False":
+    print ("Correct")
+else : print ("The answer could be only 'True' or 'False'")
+print ("Daniel informs about the reason he wants to work for this company?")
+answer4 = input()
+if answer4 == "True":
+    print ("Correct")
+elif answer4 == "False":
+    print ("Wrong")
+else : print ("The answer could be only 'True' or 'False'")    
+print ("The resons why you want to work for the company should concern only your pesonal development not company's?")
+answer5 = input()
+if answer5 == "True":
+    print ("wrong")
+elif answer5 == "False":
+    print ("Correct")
+else : print ("The answer could be only 'True' or 'False'")    
